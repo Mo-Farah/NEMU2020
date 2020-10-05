241b57ec106fc75446777f1ce5956095882eac45
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-05 12:49:32 +0000

    >  compile NEMU 3019244163 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:49:32 up  2:35,  1 user,  load average: 1.23, 1.10, 0.92 1d23f67078de439522602ac2374132c7c8da9395

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-	NOTYPE = 256, EQ,NEQ,NUMBER,HNUMBER,REGISTER,AND,OR
+	NOTYPE = 256, EQ,NEQ,NUMBER,HNUMBER,REGISTER,AND,OR,MARK,POINTOR,MINUS,R_DS
 
 	/* TODO: Add more token types */
 
@@ -115,6 +115,118 @@ static bool make_token(char *e) {
 
 	return true; 
 }
+bool check_similar(int u,int v)
+{
+	int i;
+	if(token[u].type == '('&&token[v].type ==')')
+	{
+		int uc=0,vc=0;
+		for(i=u+1;i<v;i++)
+		{
+			if(token[i].type == '(')uc++;
+			if(token[i].type ==')')vc++;
+			if(vc>uc)return false;
+		}
+		if(uc==vc)return true;
+	}
+	return false;
+}
+int dominant_operator(int l,int r)
+{
+	int i,j;
+	int min_priority=10;
+	int oper=l;
+	for(i=l;i<=r;i++)
+	{
+		if(token[i].type==NUMBER||token[i].type==HNUMBER||token[i].type==REGISTER||token[i].type==MARK)
+			continue;
+		int cnt=0;
+		bool key=true;
+		for(j=i-1;j>=l;j--)
+		{
+			if(token[j].type=='('&&!cnt){key =false;break;}
+			if(token[j].type=='(')cnt --;
+			if(token[j].type==')')cnt ++;
+		}
+		if(!key)continue;
+		if(token[i].priority<=min_priority){min_priority=token[i].priority;oper=i;}
+	}
+	return oper;
+}
+uint32_t eval(int l,int r){
+	if(l>r){Assert(l>r,"something happened!\n");return 0;}
+	if(l==r){
+	uint32_t num=0;
+	if(token[l].type==NUMBER)
+		sscanf(token[l].str,"%d",&num);
+	if(token[l].type==HNUMBER)
+		sscanf(token[l].str,"%x",&num);
+	if(token[l].type==REGISTER)
+		{
+			if(strlen(token[l].str)==3){
+			int i;
+			for(i=R_EAX;i<=R_EDI;i++)
+				if(strcmp(token[l].str,regsl[i])==0)break;
+				if(i>R_EDI)
+				if(strcmp(token[l].str,"eip")==0)
+					num=cpu.eip;
+				else Assert(1,"no this register!\n");
+			else num =reg_l(i);
+			
+			}
+			else if(strlen(token[l].str)==2){
+			if(token[l].str[1]=='x'||token[l].str[1]=='p'||token[l].str[1]=='i'){
+				int i;
+				for(i=R_AX;i<=R_DI;i++)
+					if(strcmp(token[l].str,regsw[i])==0)break;
+				num=reg_w(i);
+			}
+			else if(token[l].str[1]=='l'||token[l].str[1]=='h'){
+				int i;
+				for(i=R_AL;i<=R_BH;i++)
+					if(strcmp(token[l].str,regsb[i])==0)break;
+				num=reg_b(i);
+			}
+			else assert(1);
+		}
+		return num;
+	}
+	}
+	else if(check_similar(l,r)==true)return eval(l+1,r-1);
+	else {
+		int op=dominant_operator(l,r);
+		if(l==op||token[op].type==POINTOR||token[op].type==MINUS||token[op].type=='!')
+		{
+			uint32_t val=eval(l+1,r);
+			switch(token[l].type)
+			{
+				case POINTOR: ;return swaddr_read(val,4);
+				case MINUS:return -val;
+				case '!':return !val;
+				default :Assert(1,"default\n");
+			}
+		}
+		uint32_t val1=eval(l,op-1);
+		uint32_t val2=eval(op+1,r);
+		switch(token[op].type)
+		{
+			case '+':return val1+val2;
+			case '-':return val1-val2;
+			case '*':return val1*val2;
+			case '/':return val1/val2;
+			case  EQ:return val1==val2;
+			case NEQ:return val1!=val2;
+			case AND:return val1&&val2;
+			case OR:return val1||val2;
+			default:
+			break;
+		}
+	}
+		assert(1);
+		return -123456;
+
+}
+
 
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
@@ -123,7 +235,6 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	*success = true;	
+	return eval(0,nr_token-1);
 }
-
